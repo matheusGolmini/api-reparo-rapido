8d12df3b3b5a5e44f651ec326d048e5dfafaffd0
Matheus <matheus>
2021-07-20 20:29:08 -0300

    refactor: remove import not used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientApiModule } from './client-api/client-api.module';
-import { ProviderApiModule } from './provider-api/provider-api.module';
-import { AdminApiModule } from './admin-api/admin-api.module';
 import { ClientModule } from './client/client.module';
 import { ProviderModule } from './provider/provider.module';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [ClientApiModule, ProviderApiModule, AdminApiModule, ClientModule, ProviderModule, AdminModule],
+  imports: [ClientModule, ProviderModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
