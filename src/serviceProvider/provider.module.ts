cd32962370e475dc66b8b86ee7cd76e3b5cc0daf
Matheus <matheus>
2021-07-28 22:43:01 -0300

    refatcor: change file name

---
+++ src/serviceProvider/provider.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+// import { ProviderService } from './service/service-provider.service';
+// import { ProviderController } from './controller/service-provider.controller';
+
+@Module({
+  // controllers: [ProviderController],
+  // providers: [ProviderService],
+})
+export class ProviderModule {}
---
+++ src/serviceProvider/service/service-provider.service.ts
@@ -0,0 +1,24 @@
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class ProviderService {
+//   create(createProviderDto: any) {
+//     return 'This action adds a new provider';
+//   }
+
+//   findAll() {
+//     return `This action returns all provider`;
+//   }
+
+//   findOne(id: number) {
+//     return `This action returns a #${id} provider`;
+//   }
+
+//   update(id: number, updateProviderDto: UpdateProviderDto) {
+//     return `This action updates a #${id} provider`;
+//   }
+
+//   remove(id: number) {
+//     return `This action removes a #${id} provider`;
+//   }
+// }
