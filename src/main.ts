299ec1d80b2b708caf00bb4e540c7240f96761a8
Matheus <matheus>
2021-08-04 20:46:34 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
+  });
 
   const config = new DocumentBuilder()
     .addBearerAuth()
