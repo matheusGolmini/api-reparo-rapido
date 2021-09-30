e11fd1a08b97fe1e8a59cb91b4f5e1a09b4171e9
matheus <matheussouza@vipcommerce.com.br>
2021-09-30 20:10:45 -0300

    upalod

--- src/upload/controller/upload.controller.ts
+++ src/upload/controller/upload.controller.ts
@@ -1,18 +1,14 @@
 import {
   Controller,
   Post,
-  UseGuards,
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
 import { UploadService } from '../service/upload.service';
-import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('Upload')
-@UseGuards(JwtAuthGuard)
-@ApiBearerAuth()
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
